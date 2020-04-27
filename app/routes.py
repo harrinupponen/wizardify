19a91bbb0f8e633a27b0bed624fdf9b299c84bd3
HeiniRa <46703786+HeiniRa@users.noreply.github.com>
2020-04-27 11:50:39 +0300

    Kuvat folder added for pic saving and path changed to work different os and heroku

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@ from app import app
 from werkzeug.utils import secure_filename
 # import form from app.forms and set route for upload
 
-UPLOAD_FOLDER = '/home/torso/wizardify/app/downlooood'
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__),"kuvat")
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -36,15 +37,16 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             #filename to random string maybe. gotta keep extensions!
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fullpath = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(fullpath)
             return redirect(url_for('uploaded_file', filename=filename))
+
     return url_for('uploaded_file', filename=filename)
 
     
 @app.route('/downlooood/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 @app.route('/result')
 def result():
