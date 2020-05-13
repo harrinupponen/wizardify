97aabc176af03434eb3b637d5e989ed4312f0dcd
Intomies <valtteri.nummi@gmail.com>
2020-05-13 10:55:34 +0300

    commented the code in predict.js

--- TFJS/static/predict.js
+++ TFJS/static/predict.js
@@ -1,3 +1,4 @@
+//This reads the users uploaded image and sets it to be visible on screen 
 $('#image-selector').change(function() {
     let reader = new FileReader();
     reader.onload = function() {
@@ -9,12 +10,14 @@ $('#image-selector').change(function() {
     reader.readAsDataURL(file);
 });
 
+//This loads the model from set path
 let model;
 (async function() {
     model = await tf.loadLayersModel('./models/final_model/model.json');
     $('.progress-bar').hide();
 })();
 
+//This makes the prediction using user image and model
 $('#predict-button').click(async function() {
     let image = $('#selected-image').get(0);
     let offset = tf.scalar(127.5);
@@ -25,6 +28,7 @@ $('#predict-button').click(async function() {
         .div(offset)
         .expandDims();
 
+    //This forms the prediction data, sorts it and prints the top 5 results on screen
     let predictions = await model.predict(tensor).data();
     let top5 = Array.from(predictions).map(function (p, i) {
             return {
